4f6623b7e31e5e0c52074717fec3a5799c15c2c3
Sonia Syed <sonia.faiyaz@gmail.com>
2022-06-14 06:17:26 +0300

    Login and Signup forms

---
+++ login.php
@@ -0,0 +1,55 @@
+
+
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" type="text/css" href="">
+<title>Dashboard login</title>
+
+</head>
+<style> input, td {font-size:39px;} </style>
+<body  style="background-image:url('lights.jpg');font-size:39px">
+
+	
+
+	<div class="">
+
+		<h2 style=" text-align: centre;">
+			WELCOME <br>Thank you for joining us
+		</h2>
+	
+		<div align="center">
+			
+			
+			
+			<form method="post" action="login.php">
+			
+				<table align="center">
+					<tr>
+						<td>User Name</td>
+</tr><tr>
+						<td><input type="text" name="username" required="required"/></td>
+		</tr>
+<tr>
+					
+						<td>Password</td>
+</tr>
+<tr>
+						<td><input type="password" name="password" required="required"/></td>
+					</tr>
+					<tr>
+						<td align="center" colspan="2"><input type="submit" name="submit" value="Login" /></td>
+					</tr>
+				</table>
+				<a href="registration.php" style="color: #045D4D">Don't have an account? Register Here</a>
+			</form>
+		</div>
+		<br/><br/><br/><br/>
+
+	</div>
+	
+
+</body>
+</html>
